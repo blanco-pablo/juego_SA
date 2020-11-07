e4f638f3fa358fd0cfd39ddf672df01335efc48c
blanco-pablo <blancocalderonpablo@gmail.com>
2020-11-07 10:33:38 +0000

    SONAR

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner =  require('sonarqube-scanner');
 sonarqubeScanner(
     {
-        serverUrl:  'http://localhost:9000',
+        serverUrl:  'http://34.68.244.180:9000/',
         options : {
             'sonar.sources':  'src',
             'sonar.tests':  'src',
